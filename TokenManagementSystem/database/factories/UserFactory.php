6e0680fdad7dcefa0ffa061c418a6340e7357785
divyanshu <dgdivyanshu@gmail.com>
2019-09-22 19:30:18 +0530

    Added database seeding

--- TokenManagementSystem/database/factories/UserFactory.php
+++ TokenManagementSystem/database/factories/UserFactory.php
@@ -18,10 +18,11 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        //'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'type'=>['Student','Teacher'][rand(0,1)],
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'secret1234', // password
         'remember_token' => Str::random(10),
     ];
 });
