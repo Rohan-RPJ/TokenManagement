13787d37ee042ef12ecfd22cebc0ed34beca5602
divyanshu <dgdivyanshu@gmail.com>
2019-10-01 22:18:26 +0530

    Radio button getting reset every time new createQuestionInput is called

--- TokenManagementSystem/resources/views/round/startround.blade.php
+++ TokenManagementSystem/resources/views/round/startround.blade.php
@@ -10,10 +10,18 @@
 		<h2>{{$submission->subject->name}}</h2>
 	</div>
 
+	<div class="timer">	
+
+	</div>
+
 	<div class=center-align>
 		<h5>
 			<span id="start-message"><strong>PLEASE KEEP THIS PAGE OPEN .ROUND WILL BEGIN SOON</strong></span>
 		</h5>
+		<h5>
+			<div id="question_display"></div>
+		</h5>
+		
 	</div>
 </div>
 
@@ -26,6 +34,8 @@
 		var roundData=null;
 		var question_form=null;
 
+		var timer_sec=5;
+
 		function getRoundStatus(){
 					console.log("Executing getRoundStatus");
 
@@ -34,6 +44,7 @@
 					result=data.result;
 					if(result == true)
 					{
+
 						console.log("Round can be started");
 						$("#start-message").text("ROUND IS  NOW STARTING....");
 						clearInterval(checkroundstatus);
@@ -41,13 +52,26 @@
 						//fetching the round_data now
 						console.log("Round data"+roundData);
 						console.log(roundData);
-						console.log(roundData.q1);
+						
+						console.log(roundData.q2);
 						 //creating an invisible form now
 						 question_form = document.createElement('form');
 						 question_form.name = "response";
 
 
-						 createQuestionInput(1,1);
+						 // for(var i=1;questionsIterator(i,roundData);i++){
+						 // 		var q="q"+i;
+							// 	 createQuestionInput(i,roundData[q]);
+						 // }
+						 var q="q",i=1;
+
+						 var x=setInterval(function(){
+						 							createQuestionInput(i,roundData[q+i])
+						 							i=i+1;
+						 							if(!questionsIterator(i,roundData))
+						 								{clearInterval(x);}
+						 				},timer_sec*1000);
+						 //createQuestionInput(1,1);
 
 					}
 				
@@ -56,9 +80,34 @@
 
 	checkroundstatus = window.setInterval(getRoundStatus ,1000);
 
+	function questionsIterator(i,data){
+			var q="q"+i;
+			if (q in data){return true;}
+			return false;
+	}
 
 	function getRoundData(){
 			roundData={!! json_encode($round_id->toArray(), JSON_HEX_TAG) !!};
+	}
+
+	function uncheck(){
+		var parent=this;
+		console.log(parent);
+		console.log("Parent id:"+parent.id);
+		var radiogroup	= parent.id.split("_")[1];
+		
+		radiogroup = document.getElementsByName("answer_"+radiogroup);
+		console.log(radiogroup);
+
+		for(var i=0;i<radiogroup.length;i++)
+			{
+				if(radiogroup[i].checked){
+					radiogroup[i].checked=false;
+					console.log("Changed checked to false for option"+(i+1));
+					break;
+
+				}
+			}
 	}
 
 	function createQuestionInput(questionNo,question_id){
@@ -81,38 +130,36 @@
 
 
 									var card_html='<div class="row">\
-									    <div class="col s12 m6">\
+									    <div class="col s12 m6 l12">\
 					      				<div class="card blue-grey darken-1">\
 					        					<div class="card-content white-text">\
 					          							<span class="card-title"> Question '+questionNo+'</span>\
 					          									<p>'+question_description+'</p>\
-					        					</div>\
-					        					<div class="card-action">\
-					          						<a href="#">'+option1+'</a>\
-					        					</div>\
-					        					<div class="card-action">\
-					          						<a href="#">'+option2+'</a>\
-					        					</div>\
-					        					<div class="card-action">\
-					          						<a href="#">'+option3+'</a>\
-					        					</div>\
-					        					<div class="card-action">\
-					          						<a href="#">'+option4+'</a>\
-					        					</div>\
-					      					  </div>\
+					        					</div>'
+					        					+makeOptionDiv(question_id,option1,1)
+					        					+makeOptionDiv(question_id,option2,2)
+					        					+makeOptionDiv(question_id,option3,3)
+					        					+makeOptionDiv(question_id,option4,4)
+					        					+'<div class="card-action"><a class="btn-flat waves-teal wave-effect"  id="clear_'+question_id+'">CLEAR</a></div>'+
+					      					  '</div>\
 					    					</div>\
 					  					</div>';
 
-				  						document.getElementById("start-message").innerHTML=card_html;
+				  						document.getElementById("question_display").innerHTML+=card_html;
+				  						var clr_btn=document.getElementById("clear_"+question_id);
+				  						clr_btn.addEventListener("click",uncheck,false);
 								});
 		
 	}
 
-	//createQuestionInput();
+function makeOptionDiv(question_id,option,option_no){
+	var inputdiv=`<label><input type="radio" value=${option_no} name="answer_${question_id}" /><span>${option}</span></label>`;
+	var optiondiv= '<div class="card-action">'+inputdiv+'</div>';
+
+	return optiondiv;
+}
 //	getRoundData();
-	
-		
-	
+
 </script>	
 
 @endsection
